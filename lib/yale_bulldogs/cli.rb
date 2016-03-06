3ff2e2b5e95e9e55a0446caa16cc50886ee62b31
Colton Staab <colton.staab@gmail.com>
2016-03-06 11:35:59 -0800

    App meets all of my requirements, but could probably use refactoring...

--- lib/yale_bulldogs/cli.rb
+++ lib/yale_bulldogs/cli.rb
@@ -3,32 +3,40 @@ class YaleBulldogs::CLI
 	def call
 		puts "Welcome to the Yale Swimming and Diving CLI app"
 		puts "Please enter 'exit' at any time to quit the app"
-		meet_information(doc)
+		meet_information
 		goodbye
 	end
 
 	def request_gender
-		puts "Are you looking for information on the men's team or women's team?:"
-		gender_input = gets.strip.downcase
-		if gender_input == 'm' || gender_input =='men' || gender_input == 'mens' || gender_input == "men's" 
-			gender = 'm'
-		elsif gender_input == 'w' || gender_input == 'women' || gender_input == 'womens' || gender_input == "women's" 
-			gender = 'w'
-		else
-			puts "Input invalid. Please try again."
-			request_gender
+		gender_input = nil
+		while gender_input != 'exit'
+			puts "Are you looking for information on the men's team or women's team?:"
+			gender_input = gets.strip.downcase
+			if gender_input == 'm' || gender_input =='men' || gender_input == 'mens' || gender_input == "men's" 
+				gender = 'm'
+			elsif gender_input == 'w' || gender_input == 'women' || gender_input == 'womens' || gender_input == "women's" 
+				gender = 'w'
+			elsif gender_input == 'exit'
+				abort("Bulldogs, bulldogs, bow bow bow, Eli Yale!")
+			else
+				puts "Input invalid. Please try again."
+				request_gender
+			end
+			return gender
 		end
-		gender
 	end
 
 	def request_year
-		puts "What year are you interested in?:"
-		year = gets.strip
-		if year.to_s.length != 4
-			puts "Input invalid. Please try again."
-			request_year
-		end	
-		year
+		year = nil
+		while year != 'exit'
+			puts "What year are you interested in? (input must be in YYYY format):"
+			year = gets.strip
+			if year.to_s.length != 4
+				puts "Input invalid. Please try again."
+				request_year
+			end	
+			return year
+		end
 	end
 
 	def get_year_range(year)
@@ -48,7 +56,7 @@ class YaleBulldogs::CLI
 		end
 	end
 
-	def display_meets(doc)
+	def display_meets
 		doc = request_data
 		results = YaleBulldogs::Season.scrape_season(doc)
 		puts results[0].text
@@ -58,25 +66,25 @@ class YaleBulldogs::CLI
 		results[1]
 	end
 
-	def meet_information(doc)
-		meets = display_meets(doc)
+	def meet_information
+		meets = display_meets
 		input = nil
 		while input != 'exit' && input != 'back'
 			puts "Which meet are you interested in? (enter meet number):"
-			begin
-				input = gets.strip
+			input = gets.strip
+			if input.to_i > 0 && input.to_i <= meets.length
 				meet = meets[input.to_i - 1]
 				puts "Opponent: " + meet.opponent
 				puts "Date: " + meet.date
 				puts "Time: " + meet.time
 				puts "Result: " + meet.result
-			rescue
+			elsif input == 'back'
+				call
+			elsif input == 'exit'
+				break
+			else
 				puts "Number invalid. Please try again:"
-				meet_information(doc)
-			end	
-		end
-		if input == 'back'
-			call
+			end
 		end
 	end
 
