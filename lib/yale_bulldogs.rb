96bf9634089ad1265e5e000fb5baec05e093f5b8
Colton Staab <colton.staab@gmail.com>
2016-03-06 11:41:44 -0800

    removed irb.start from yale-bulldogs.rb

--- lib/yale_bulldogs.rb
+++ lib/yale_bulldogs.rb
@@ -1,8 +1,3 @@
-=begin
-require 'bundler/setup'
-require 'yale_bulldogs'
-=end
-
 require_relative "./yale_bulldogs/version"
 require_relative './yale_bulldogs/cli.rb'
 require_relative "./yale_bulldogs/season.rb"
@@ -11,5 +6,3 @@ require_relative "./yale_bulldogs/meet.rb"
 require 'irb'
 require 'nokogiri'
 require 'open-uri'
-
-IRB.start
